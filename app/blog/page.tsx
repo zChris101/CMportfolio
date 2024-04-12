920bdedcd42f4332da21bfe07669fc52072f344a
zchris101 <chrstnmusico@gmail.com>
2024-04-12 16:34:09 +0800

    Navbar Edited

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 import { title } from "@/components/primitives";
 
-export default function BlogPage() {
+export default function PricingPage() {
 	return (
 		<div>
 			<h1 className={title()}>Blog</h1>
--- config/site.ts
+++ config/site.ts
@@ -1,69 +1,54 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
-	navItems: [
-		{
-			label: "Home",
-			href: "/",
-		},
+  name: "Christian Musico",
+  description: "Christian Musico's personal web portfolio ",
+  navItems: [
     {
-      label: "Docs",
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "About",
       href: "/docs",
     },
     {
-      label: "Pricing",
+      label: "Skills",
       href: "/pricing",
     },
     {
-      label: "Blog",
+      label: "Projects",
       href: "/blog",
+    },
+    {
+      label: "Blog",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
     },
     {
       label: "About",
       href: "/about",
-    }
-	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
-	],
-	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui.org",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
-	},
+    },
+    {
+      label: "Skills",
+      href: "/skills",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+  
+  ],
+  links: {
+    github: "https://github.com/nextui-org/nextui",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui.org",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
 };
