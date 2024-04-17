d31f7a4d11ad77e84b2591b4893ce5bcb7ef762e
zchris101 <chrstnmusico@gmail.com>
2024-04-17 17:42:10 +0800

    rework

--- app/page.tsx
+++ app/page.tsx
@@ -9,44 +9,16 @@ import { Image } from "@nextui-org/image";
 
 export default function Home() {
   return (
-    <section
-      className="
-      flex-col-reverse
-      flex
-      items-center
-      justify-items-center
-      sm:flex-row
-      sm:justify-between
-      sm:ml-5
-      sm:mr-10
-      sm:items-center
+    <div>
+      <div className="
+       border-3
+       w-full
+       h-screen
       "
-    >
-      <div className="sm:gap-20 sm:justify-between">
-        
-          <h1
-            className="
-            my-3
-            mx-4
-            text-3xl
-            font-extrabold
-            "
-          >
-            Hi there!
-          </h1>
-        
-       
-          <p className="text-justify mx-4 sm:text-x sm:mr-60 sm:text-justify">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut fugit
-            eius illo et? Rem, harum. Consectetur, quae vitae minus, porro
-            soluta aliquam repudiandae, dolores sit rem repellendus consequuntur
-            sapiente dolor?
-          </p>
-        
+      >
+
       </div>
-      <div className="-mt-10">
-        <Image alt="dummy bg" width={320} height={300} src="/bgpicdvo.jpg" />
-      </div>
-    </section>
+     
+    </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -40,7 +40,7 @@ export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <NextUINavbar>
+    <NextUINavbar shouldHideOnScroll={false} position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
